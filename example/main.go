45299dd4e8300b4eafb81c58b1660bcfd6714116
Chris Sexton <EMAIL>
2014-02-08 22:30:47 -0500

    Add dvorak bindings to example

--- example/main.go
+++ example/main.go
@@ -1,22 +1,47 @@
-// © 2014 the grlutil Authors under the WTFPL. See AUTHORS for the list of authors.
+// © 2014 the grl Authors under the WTFPL. See AUTHORS for the list of authors.
 
 package main
 
 import (
-        "fmt"
-        "math/rand"
+	"flag"
+	"fmt"
+	"math/rand"
 
-        rlutil "github.com/chrissexton/grlutil"
+	rl "github.com/chrissexton/grlutil"
 )
 
 const (
-        FLOOR = iota
-        WALL
-        COIN
-        STAIRS_DOWN
-        TORCH
-        MAPSIZE = 15
+	FLOOR = iota
+	WALL
+	COIN
+	STAIRS_DOWN
+	TORCH
+	MAPSIZE = 15
 )
+
+type keymap struct {
+	up    rune
+	down  rune
+	left  rune
+	right rune
+	quit  rune
+}
+
+var qwerty keymap = keymap{
+	up:    'w',
+	down:  's',
+	left:  'a',
+	right: 'd',
+	quit:  27,
+}
+
+var dvorak keymap = keymap{
+	up:    ',',
+	down:  'o',
+	left:  'a',
+	right: 'e',
+	quit:  27,
+}
 
 var x, y int
 var coins, moves, torch, level int = 0, 0, 30, 1
@@ -24,132 +49,134 @@ var lvl [MAPSIZE][MAPSIZE]int
 
 /// Generates the dungeon map
 func gen(seed int) {
-        rand.Seed(int64(seed))
-        for j := 0; j < MAPSIZE; j++ {
-                for i := 0; i < MAPSIZE; i++ {
-                        if i == 0 || i == MAPSIZE-1 || j == 0 || j == MAPSIZE-1 || rand.Int()%10 == 0 {
-                                lvl[i][j] = 1
-                        } else if rand.Int()%20 == 0 {
-                                lvl[i][j] = COIN
-                        } else if rand.Int()%100 == 0 {
-                                lvl[i][j] = TORCH
-                        } else {
-                                lvl[i][j] = 0
-                        }
-                }
-        }
-        var randcoord int
-        x = 1 + rand.Int()%MAPSIZE - 2
-        y = 1 + rand.Int()%MAPSIZE - 2
-        lvl[randcoord][randcoord] = STAIRS_DOWN
+	rand.Seed(int64(seed))
+	for j := 0; j < MAPSIZE; j++ {
+		for i := 0; i < MAPSIZE; i++ {
+			if i == 0 || i == MAPSIZE-1 || j == 0 || j == MAPSIZE-1 || rand.Int()%10 == 0 {
+				lvl[i][j] = 1
+			} else if rand.Int()%20 == 0 {
+				lvl[i][j] = COIN
+			} else if rand.Int()%100 == 0 {
+				lvl[i][j] = TORCH
+			} else {
+				lvl[i][j] = 0
+			}
+		}
+	}
+	var randcoord int
+	x = 1 + rand.Int()%MAPSIZE - 2
+	y = 1 + rand.Int()%MAPSIZE - 2
+	lvl[randcoord][randcoord] = STAIRS_DOWN
 }
 
 /// Draws the screen
 func draw() {
-        rlutil.Cls()
-        rlutil.Locate(1, MAPSIZE+1)
-        rlutil.SetColor(rlutil.YELLOW)
-        fmt.Printf("Coins: %d\n", coins)
-        rlutil.SetColor(rlutil.RED)
-        fmt.Printf("Torch: %d\n", torch)
-        rlutil.SetColor(rlutil.MAGENTA)
-        fmt.Printf("Moves: %d\n", moves)
-        rlutil.SetColor(rlutil.GREEN)
-        fmt.Printf("Level: %d\n", level)
-        rlutil.Locate(1, 1)
-        abs := func(val int) int {
-                if val < 0 {
-                        return -1 * val
-                }
-                return val
-        }
-        min := func(x, y int) int {
-                if x < y {
-                        return x
-                }
-                return y
-        }
-        for j := 0; j < MAPSIZE; j++ {
-                for i := 0; i < MAPSIZE; i++ {
-                        if abs(x-i)+abs(y-j) > min(10, torch/2) {
-                                fmt.Printf(" ")
-                        } else if lvl[i][j] == 0 {
-                                rlutil.SetColor(rlutil.BLUE)
-                                fmt.Printf(".")
-                        } else if lvl[i][j]&WALL != 0 {
-                                rlutil.SetColor(rlutil.CYAN)
-                                fmt.Printf("#")
-                        } else if lvl[i][j]&COIN != 0 {
-                                rlutil.SetColor(rlutil.YELLOW)
-                                fmt.Printf("o")
-                        } else if lvl[i][j]&STAIRS_DOWN != 0 {
-                                rlutil.SetColor(rlutil.GREEN)
-                                fmt.Printf("<")
-                        } else if lvl[i][j]&TORCH != 0 {
-                                rlutil.SetColor(rlutil.RED)
-                                fmt.Printf("f")
-                        }
-                }
-                fmt.Printf("\n")
-        }
-        rlutil.Locate(x+1, y+1)
-        rlutil.SetColor(rlutil.WHITE)
-        fmt.Printf("@")
+	rl.Cls()
+	rl.Locate(1, MAPSIZE+1)
+	rl.SetColor(rl.YELLOW)
+	fmt.Printf("Coins: %d\n", coins)
+	rl.SetColor(rl.RED)
+	fmt.Printf("Torch: %d\n", torch)
+	rl.SetColor(rl.MAGENTA)
+	fmt.Printf("Moves: %d\n", moves)
+	rl.SetColor(rl.GREEN)
+	fmt.Printf("Level: %d\n", level)
+	rl.Locate(1, 1)
+	for j := 0; j < MAPSIZE; j++ {
+		for i := 0; i < MAPSIZE; i++ {
+			if rl.Abs(x-i)+rl.Abs(y-j) > rl.Min(10, torch/2) {
+				fmt.Printf(" ")
+			} else if lvl[i][j] == 0 {
+				rl.SetColor(rl.BLUE)
+				fmt.Printf(".")
+			} else if lvl[i][j]&WALL != 0 {
+				rl.SetColor(rl.CYAN)
+				fmt.Printf("#")
+			} else if lvl[i][j]&COIN != 0 {
+				rl.SetColor(rl.YELLOW)
+				fmt.Printf("o")
+			} else if lvl[i][j]&STAIRS_DOWN != 0 {
+				rl.SetColor(rl.GREEN)
+				fmt.Printf("<")
+			} else if lvl[i][j]&TORCH != 0 {
+				rl.SetColor(rl.RED)
+				fmt.Printf("f")
+			}
+		}
+		fmt.Printf("\n")
+	}
+	rl.Locate(x+1, y+1)
+	rl.SetColor(rl.WHITE)
+	fmt.Printf("@")
 }
 
 /// Main loop and input handling
 func main() {
-        rlutil.HideCursor()
-        gen(level)
-        rlutil.SetColor(2)
-        fmt.Printf("Welcome! Use WASD to move.\n")
-        rlutil.SetColor(6)
-        fmt.Printf("Hit any key to start.\n")
-        rlutil.AnyKey()
-        draw()
-        for true {
-                // Input
-                if rlutil.KbHit() {
-                        var k = rlutil.GetCh()
+	useDvorak := flag.Bool("dvorak", false, "Use dvorak keybindings (,oae)")
+	flag.Parse()
 
-                        oldx, oldy := x, y
-                        if k == 'a' {
-                                x--
-                                moves++
-                        } else if k == 'd' {
-                                x++
-                                moves++
-                        } else if k == 'w' {
-                                y--
-                                moves++
-                        } else if k == 's' {
-                                y++
-                                moves++
-                        } else if k == 27 {
-                                break
-                        }
-                        // Collisions
-                        if lvl[x][y]&WALL != 0 {
-                                x = oldx
-                                y = oldy
-                        } else if lvl[x][y]&COIN != 0 {
-                                coins++
-                                lvl[x][y] ^= COIN
-                        } else if lvl[x][y]&TORCH != 0 {
-                                torch += 20
-                                lvl[x][y] ^= TORCH
-                        } else if lvl[x][y]&STAIRS_DOWN != 0 {
-                                level++
-                                gen(level)
-                        }
-                        // Drawing
-                        draw()
-                        // Die
-                        torch--
-                        if torch <= 0 {
-                                break
-                        }
-                }
-        }
-        rlutil.ShowCursor()
+	keys := qwerty
+	if *useDvorak {
+		keys = dvorak
+	}
+
+	rl.HideCursor()
+	gen(level)
+	rl.SetColor(2)
+	if *useDvorak {
+		fmt.Printf("Welcome! Use ,oae (the physical WSAD keys) to move.\n")
+	} else {
+		fmt.Printf("Welcome! Use WASD to move.\n")
+	}
+	rl.SetColor(6)
+	fmt.Printf("Hit any key to start.\n")
+	rl.AnyKey()
+	draw()
+	for true {
+		// Input
+		if rl.KbHit() {
+			var k = rl.GetCh()
+
+			oldx, oldy := x, y
+			if k == keys.left {
+				x--
+				moves++
+			} else if k == keys.right {
+				x++
+				moves++
+			} else if k == keys.up {
+				y--
+				moves++
+			} else if k == keys.down {
+				y++
+				moves++
+			} else if k == 27 {
+				break
+			} else {
+				continue
+			}
+			// Collisions
+			if lvl[x][y]&WALL != 0 {
+				x = oldx
+				y = oldy
+			} else if lvl[x][y]&COIN != 0 {
+				coins++
+				lvl[x][y] ^= COIN
+			} else if lvl[x][y]&TORCH != 0 {
+				torch += 20
+				lvl[x][y] ^= TORCH
+			} else if lvl[x][y]&STAIRS_DOWN != 0 {
+				level++
+				gen(level)
+			}
+			// Drawing
+			draw()
+			// Die
+			torch--
+			if torch <= 0 {
+				break
+			}
+		}
+	}
+	rl.ShowCursor()
 }
